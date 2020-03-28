14c806ed3d3edc52370e56593dad46e89abac659
Susan Weaver <susan@weaverlogic.com>
2020-03-28 12:21:02 -0700

    adding validity checks for user responses

--- Develop/script.js
+++ Develop/script.js
@@ -2,28 +2,61 @@
 
 // Request input from the user and assign to variables.
 var lengthChoice = prompt("How many charactors would you like the password to be (min 8, max 128)?");
-   console.log(lengthChoice)
-   //check it's between 8 and 128 
-   while (lengthChoice < 8 || lengthChoice > 128) {
-      var lengthChoice = prompt("OPPS! please choose a number between 8 and 128");
-      break;
-      };
-      console.log(lengthChoice);
+  //check it's between 8 and 128 
+  while (lengthChoice < 8 || lengthChoice > 128) {
+    var lengthChoice = prompt("OOPS! please choose a number between 8 and 128");
+    console.log(lengthChoice)
+      if (lengthChoice >= 8 && lengthChoice <= 128) 
+        break;
+    };
 
-  var lowerCase = prompt("Do you want to include LOWER case letters? YES or NO");
-  console.log(lowerCase);
-  // check yes no
 
-  var upperCase = prompt("Do you want to include UPPER case letters? YES or NO");
-  console.log(upperCase);
-  //check yes no
+var lowerCaseChoice = prompt("To include LOWER CASE letters, type YES or NO");
+  //convert user response to lower case
+  l = lowerCaseChoice.toLowerCase()
+    //check to see if the answer is some version of yes or no, and if not ask for a new response
+    while (l != "yes" && l != "no") {
+      var lowerCaseChoice = prompt("OOPS! please enter YES or NO - would you like to use LOWER case letters?");
+      l = lowerCaseChoice.toLowerCase()
+    }
+    // write to user choice to the console.log for easy reference
+    console.log (lowerCaseChoice + " lower case")
 
-  var numSpecChar = prompt("Do you want to include numeric and/or special charactors case letters? YES or NO");
-  console.log(numSpecChar);
-  //check yes no
 
-// Check responses from the user to see if they are valid
+var upperCaseChoice = prompt("To include UPPER CASE letters, type YES or NO");
+  //convert user response to lower case
+  u = upperCaseChoice.toLowerCase()
+    //check to see if the answer is some version of yes or no, and if not ask for a new response
+    while (u != "yes" && u != "no") {
+      var upperCaseChoice = prompt("OOPS! please enter YES or NO - would you like to use UPPER case letters?");
+      u = upperCaseChoice.toLowerCase()
+    }
+    // write to user choice to the console.log for easy reference
+    console.log (upperCaseChoice + " upper case")
 
+
+var numCharChoice = prompt("To include NUMBERS, type YES or NO");
+  //convert user response to lower case
+  n = numCharChoice.toLowerCase()
+    //check to see if the answer is some version of yes or no, and if not ask for a new response
+    while (n != "yes" && n != "no") {
+      var numCharChoice = prompt("OOPS! please enter YES or NO - would you like to use NUMBERS?");
+      n = numCharChoice.toLowerCase()
+    }
+    // write to user choice to the console.log for easy reference
+    console.log (numCharChoice + " numbers")
+  
+
+var specCharChoice = prompt("To include SPECIAL CHARACTORS, type YES or NO");
+    //convert user response to lower case
+    s = specCharChoice.toLowerCase()
+      //check to see if the answer is some version of yes or no, and if not ask for a new response
+      while (s != "yes" && s != "no") {
+        var specCharChoice = prompt("OOPS! please enter YES or NO - would you like to use SPEICAL CHARACTORS?");
+        s = specCharChoice.toLowerCase()
+      }
+      // write to user choice to the console.log for easy reference
+      console.log (specCharChoice + " special charactors")
 
 
 
