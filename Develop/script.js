4aa370e8904923395544930131c568b4705f6246
Susan Weaver <susan@weaverlogic.com>
2020-03-28 08:19:09 -0700

    adding select criteria yes/no questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,31 @@
 // Assignment code here
 
+function selectPassCriteriaLength (lengthChoice) {
+  var lengthChoice = prompt("How many charactors would you like the password to be? Minimum of 8, Maximum of 128");
+  console.log(lengthChoice);
+  //check it's between 8 and 128
+}
+
+function selectPassCriteriaLowerCase (lowerCase) {
+  var lowerCase = prompt("Do you want to include LOWER case letters? YES or NO");
+  console.log(lowerCase);
+}
+
+function selectPassCriteriaUpperCase (upperCase) {
+  var upperCase = prompt("Do you want to include UPPER case letters?");
+  console.log(upperCase);
+  //check yes no
+}
+
+
+function selectPassCriteriaNumberSpecChar (numSpecChar) {
+  var numSpecChar = prompt("Do you want to include numeric and/or special charactors case letters?");
+  console.log(numSpecChar);
+  //check yes no
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +41,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+var Start = function () {
+    selectPassCriteriaLength ();
+    selectPassCriteriaLowerCase ();
+    selectPassCriteriaUpperCase ();
+    selectPassCriteriaNumberSpecChar ();
+}
+
+Start ();
